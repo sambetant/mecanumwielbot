5b2eb81b4d3ce8ded759c34982f8b9ce2c551eea
sambetant <104916257+sambetant@users.noreply.github.com>
2023-08-13 11:04:38 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,14 +1,50 @@
+function links () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
+}
 function stop () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 0)
     motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 0)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 0)
 }
+function dialiac () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
+}
 function rechts () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 255)
     motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 255)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 255)
+}
+function diareac2 () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 0)
+}
+function diarevo () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 255)
+}
+function rechtsom () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
+}
+function dialivo () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 0)
 }
 function vooruit () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
@@ -22,20 +58,12 @@ function achteruit () {
     motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 255)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
 }
+function linksom () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 255)
+}
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        vooruit()
-    } else {
-        stop()
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        achteruit()
-    } else {
-        stop()
-    }
-    if (input.logoIsPressed()) {
-        rechts()
-    } else {
-        stop()
-    }
+	
 })
