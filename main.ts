db964c690f7aa5ef2fd2a3aacbd3fb4d61d14aa4
sambetant <104916257+sambetant@users.noreply.github.com>
2023-08-14 21:29:27 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
 function links () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 255)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
 }
 function stop () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
@@ -17,10 +20,10 @@ function dialiac () {
     motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
 }
 function rechts () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 255)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
 }
 function diareac2 () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
@@ -47,16 +50,16 @@ function dialivo () {
     motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 0)
 }
 function vooruit () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 255)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 255)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, 255)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
 }
 function achteruit () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
-    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, 255)
-    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
 }
 function linksom () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
@@ -64,7 +67,10 @@ function linksom () {
     motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 255)
     motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, 255)
 }
+let speed = 0
+radio.setGroup(1)
 let tijd = 300
+speed = 225
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         for (let index = 0; index < 4; index++) {
